2b17e008bab6badf228c773f3bee0b556a1f1223
Bryant Biggs <bryantbiggs@gmail.com>
2022-12-30 19:51:40 -0500

    feat: Macro works for splatting out cluster version struct

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MacroMaybe
 
+1. We will want to use [`cargo-expand`](https://github.com/dtolnay/cargo-expand) to help debug: `cargo install cargo-expand`
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,41 @@
+use std::fmt;
+
+use seq_macro::seq;
+use serde::{Deserialize, Serialize};
+
+// #[derive(Clone, Copy, Debug, Display, Serialize, Deserialize)]
+// pub enum ClusterVersion {
+//     #[strum(serialize = "1.20")]
+//     V20,
+//     #[strum(serialize = "1.21")]
+//     V21,
+//     #[strum(serialize = "1.22")]
+//     V22,
+//     #[strum(serialize = "1.23")]
+//     V23,
+//     #[strum(serialize = "1.24")]
+//     V24,
+// }
+
+seq!(N in 20..=24 {
+    #[derive(Clone, Copy, Debug, Serialize, Deserialize)]
+    pub enum ClusterVersion {
+        #(
+            V~N,
+        )*
+    }
+
+    impl fmt::Display for ClusterVersion {
+        fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+            match *self {
+                #(
+                    ClusterVersion::V~N => write!(f, "1.{}", N),
+                )*
+            }
+        }
+    }
+});
+
 fn main() {
-    println!("Hello, world!");
+    println!("{}", ClusterVersion::V24);
 }
